843ea9bac09e1bb47386707b0caf9793b0cb94ac
Benjamin L. Havel <Benjamin@bens-mbp-2.home>
2018-11-30 23:18:29 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var testOne = "What Kind of variable am I";
+
+var testTwo = 256;
+
+var testThree = false;
+
+var testFour = 'true';
+
+var testFive = '128';
+
+var testSix = " ";
+
+var testOne = "hello" + "there";
